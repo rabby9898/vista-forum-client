fae3d0de67cbe6fe6b0dd6ed98bb844c5db1a110
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-04 15:18:44 +0600

    admin dashboard added

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -3,9 +3,12 @@ import { AiOutlineProfile } from "react-icons/ai";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { BsFileEarmarkPost } from "react-icons/bs";
 import useAllPosts from "../../Hook/useAllPosts/useAllPosts";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaUser, FaUsers } from "react-icons/fa";
+import { MdOutlineReport } from "react-icons/md";
+import { GrAnnounce } from "react-icons/gr";
 const Dashboard = () => {
   const [allPost] = useAllPosts();
+  const isAdmin = true;
   return (
     <div className="flex">
       <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
@@ -16,41 +19,96 @@ const Dashboard = () => {
         </div>
         <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
           <ul>
-            <li
-              role="button"
-              tabIndex="0"
-              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-            >
-              <div className="grid place-items-center mr-4">
-                <AiOutlineProfile></AiOutlineProfile>
-              </div>
+            {isAdmin ? (
+              <>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <FaUser></FaUser>
+                  </div>
 
-              <NavLink to="/dashboard/myProfile">My Profile</NavLink>
-            </li>
-            <li
-              role="button"
-              tabIndex="0"
-              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-            >
-              <div className="grid place-items-center mr-4">
-                <IoMdAddCircleOutline></IoMdAddCircleOutline>
-              </div>
+                  <NavLink to="/dashboard/adminProfile">Admin Profile</NavLink>
+                </li>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <FaUsers></FaUsers>
+                  </div>
 
-              <NavLink to="/dashboard/addPost">Add Post</NavLink>
-            </li>
-            <li
-              role="button"
-              tabIndex="0"
-              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-            >
-              <div className="grid place-items-center mr-4">
-                <BsFileEarmarkPost></BsFileEarmarkPost>
-              </div>
+                  <NavLink to="/dashboard/manageUsers">Manage Users</NavLink>
+                </li>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <MdOutlineReport />
+                  </div>
 
-              <NavLink to="/dashboard/myPost">
-                My Post({allPost.length})
-              </NavLink>
-            </li>
+                  <NavLink to="/dashboard/report">
+                    Reported Comments/Activities
+                  </NavLink>
+                </li>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <GrAnnounce></GrAnnounce>
+                  </div>
+
+                  <NavLink to="/dashboard/makeAnnounce">
+                    Make Announcement
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <AiOutlineProfile></AiOutlineProfile>
+                  </div>
+
+                  <NavLink to="/dashboard/myProfile">My Profile</NavLink>
+                </li>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <IoMdAddCircleOutline></IoMdAddCircleOutline>
+                  </div>
+
+                  <NavLink to="/dashboard/addPost">Add Post</NavLink>
+                </li>
+                <li
+                  role="button"
+                  tabIndex="0"
+                  className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+                >
+                  <div className="grid place-items-center mr-4">
+                    <BsFileEarmarkPost></BsFileEarmarkPost>
+                  </div>
+
+                  <NavLink to="/dashboard/myPost">
+                    My Post({allPost.length})
+                  </NavLink>
+                </li>
+              </>
+            )}
             {/* --------------------------------------------------------------------------------- */}
             <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
             <li
